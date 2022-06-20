1749225850f535c719f19a8464a531a34efd7753
Bryan Petzinger <bp2070@gmail.com>
2022-06-20 18:40:41 -0400

    add trpc

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { prisma } from '../db/client'
+import { trpc } from '../utils/trpc'
 
 const Home: NextPage = (props: any) => {
+  const {data, isLoading} = trpc.useQuery(["hello"]);
+
+  if(!data || isLoading) return <div>Loading...</div>;
+
+  return <div>{data.greeting}</div>;
+
   return (
     <div>
       <Head>
