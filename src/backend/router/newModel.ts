759c38803d7c3656ab173868b6fc340e102a8c5c
Bryan Petzinger <bp2070@gmail.com>
2022-06-20 20:59:39 -0400

    create new item

--- src/backend/router/newModel.ts
+++ src/backend/router/newModel.ts
@@ -1,11 +1,20 @@
-import * as trpc from '@trpc/server';
-import { z } from 'zod';
-import { prisma } from '../../db/client';
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import { prisma } from "../../db/client";
 
-export const newModelRouter = trpc
-  .router()
-  .query('get-all', {
-      async resolve() {
-          return await prisma.newModel.findMany()
-      }
-  });
+export const newModelRouter = trpc.router().query("get-all", {
+  async resolve() {
+    return await prisma.newModel.findMany();
+  },
+}).mutation("create", {
+    input: z.object({
+        name: z.string().min(3).max(500),
+    }),
+    async resolve({input}) {
+        const n = await prisma.newModel.create({
+            data: {
+                name: input.name,
+            }
+        })
+    }
+});
